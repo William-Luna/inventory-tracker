0b23e5f4f47e436fbb196adcf5efb25a7c701fd7
williamluna <wluna98@gmail.com>
2026-05-13 01:00:20 -0700

    get unsold items added to controller

--- backend/src/main/java/com/inventorytracker/inventory_tracker/Item/ItemController.java
+++ backend/src/main/java/com/inventorytracker/inventory_tracker/Item/ItemController.java
@@ -25,10 +25,12 @@ public class ItemController {
             @RequestParam(required = false) String name,
             @RequestParam(required = false) Date dateSoldBefore,
             @RequestParam(required = false) Date dateSoldAfter,
-            @RequestParam(required = false) String category) {
-        if (category != null) return itemService.getItemsByCategory(category);
-        else if (dateSoldBefore != null && dateSoldAfter != null) return itemService.getSoldItems(dateSoldBefore, dateSoldAfter);
-        else if (name != null) return itemService.getItemsByName(name);
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Boolean unsold) {
+        if (Boolean.TRUE.equals(unsold)) return itemService.getUnsoldItems(); //unsold param
+        else if (category != null) return itemService.getItemsByCategory(category); //category param
+        else if (dateSoldBefore != null && dateSoldAfter != null) return itemService.getSoldItems(dateSoldBefore, dateSoldAfter); //sold items between dates param
+        else if (name != null) return itemService.getItemsByName(name); //specific name param
         else return itemService.getItems();
     }
 
